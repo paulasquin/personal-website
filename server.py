1be3f97a60f95743692435154344c076c3c191f8
Paul <paul.asquin@gmail.com>
2018-12-05 14:45:05 +0100

    up first structure of website inspired by mosquito monitoring project

---
+++ server.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, request, render_template
+from flask_sslify import SSLify
+
+app = Flask(__name__)
+
+KEY_PATH = "privkey.pem"
+CRT_PATH = "fullchain.pem"
+
+
+
+@app.route("/")
+def renderHTML():
+    """
+    Render home page
+    :return: home.html
+    """
+    return render_template("pages/home.html")
+
+
+@app.route("/mosquito")
+def renderInfo():
+    """
+    :return: the mosquito-monitoring project
+    """
+    pass
+
+if __name__ == "__main__":
+    """
+    Launch flask application, with SSL certificate if available
+    """
+    if os.path.exists(CRT_PATH) and os.path.exists(KEY_PATH):
+        print("Loading with certificate")
+        # Forcing https:// connections
+        sslify = SSLify(app)
+        # Running the app with certificates
+        app.run(host='0.0.0.0', ssl_context=(CRT_PATH, KEY_PATH))
+    else:
+        print("Loading HTTP")
+        app.run(host='0.0.0.0')
